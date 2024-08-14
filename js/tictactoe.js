51c204d9e296f41ec13ac1f67639d995d52ba815
rydextrex-ng <rydextrex@gmail.com>
2024-08-14 15:57:09 +0800

    Update tictactoe.js

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -260,7 +260,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 if ('serviceWorker' in navigator) {
        window.addEventListener('load', () => {
-         navigator.serviceWorker.register('/sw.js')
+         navigator.serviceWorker.register('/js/sw.js')
            .then((registration) => {
              console.log('ServiceWorker registration successful with scope: ', registration.scope);
            })
